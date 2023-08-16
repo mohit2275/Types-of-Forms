965acadc8e8ddbd33867c4e680696595206d473f
mohit2275 <mohit.chauhan2275@gmail.com>
2023-08-16 22:29:20 +0530

    Modal Login Form

---
+++ Login Form/Modal Login Form/script.js
@@ -0,0 +1,25 @@
+let form = document.getElementById('form');
+let closeBTn = document.getElementById('openBtn');
+
+
+function showForm() {
+    form.style.display = "block";
+    document.body.style.backgroundColor = 'rgba(182, 182, 182,.8)';
+}
+
+function closeFrom() {
+    form.style.display = "none";
+    document.body.style.backgroundColor = 'white';
+}
+
+
+
+
+
+
+
+
+
+
+
+
